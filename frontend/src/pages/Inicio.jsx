90968d53f0105f5ab78d45219f3401875b10acc2
lizq237 <lizquiroz237@gmail.com>
2026-04-01 20:54:00 -0700

    añadir artistas del momento

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -4,6 +4,41 @@ import './Inicio.css';
 
 function Inicio() {
     const username="lizzq";
+    const topartist = [
+    {
+        id:1,
+        artistname: "Taylor Swift",
+        artistrank: 1,
+        artistimage: "https://img2.rtve.es/n/16604372?w=1600"
+    },
+    {
+        id:2,
+        artistname: "One Direction",
+        artistrank: 2,
+        artistimage: "https://cdn-images.dzcdn.net/images/artist/6760dba71ba14145eec5478d8b135c55/1900x1900-000000-81-0-0.jpg"
+    },
+    {
+        id:3,
+        artistname: "Twenty One Pilots",
+        artistrank: 3,
+        artistimage: "https://media.timeout.com/images/103327219/image.jpg"
+    },
+    {
+        id:4,
+        artistname: "Junior H",
+        artistrank: 4,
+        artistimage: "https://ca-times.brightspotcdn.com/dims4/default/2d23377/2147483647/strip/true/crop/4149x2765+6+0/resize/2000x1333!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F06%2F71%2Ff8db19694bf7bbd1d3c5a5e57389%2Fjunior-h.jpg"
+    },
+    {
+        id:5,
+        artistname: "Phoebe Bridgers",
+        artistrank: 5,
+        artistimage: "https://thefader-res.cloudinary.com/private_images/w_640,c_limit,f_auto,q_auto:eco/18960007Final_xagiy6/phoebe-bridgers-cover-story-interview.jpg"
+    },
+    ];
+
+
+    
     return (
         <div className='Inicio'>
             <div className='Navbar'>
@@ -26,8 +61,22 @@ function Inicio() {
                 </header>
             </div>
             <h1 className='h1'>Hola de nuevo, {username}!</h1>
+            <section className='artistas'>
+                <div className='encabezado-artistas'>
+                    <h2 className='artistas-momento'>Top Artistas del momento</h2>
+                    <p>MORE</p>
+                </div>
 
-
+                <div className='contenedor-cuadros'>
+                    {topartist.map((artista) => (
+                        <div className='cuadro-artista'>
+                            <img src={artista.artistimage} alt="Imagen artista"/>
+                            <span className='artistname'>{artista.artistname}</span>
+                            <span className='artistrank'>#{artista.artistrank}</span>
+                        </div>
+                        ))}
+                </div>
+            </section>
         </div>
     );
     }
